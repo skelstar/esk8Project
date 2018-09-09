9ce37f0a39d57aaf84b5560c84aa30e082e3eefb
skelstar <skelstar@gmail.com>
2018-09-09 13:38:34 +1200

    map to curve

--- Examples/FSR_Test/FSR_Test.ino
+++ Examples/FSR_Test/FSR_Test.ino
@@ -4,6 +4,23 @@
 int baseLine = 0;
 
 
+/*
+	3v3
+	 |
+	 |
+	---
+	
+	10k
+
+	---
+	 |
+	 |
+	io36
+
+FSR goes across io36 and GND 
+*/
+
+
 void setup() {
 	Serial.begin(9600);
 
@@ -15,8 +32,23 @@ void setup() {
 }
 
 void loop() {
-	Serial.printf("%u - %d\n", adc1_get_voltage(ADC1_CHANNEL_0), map(adc1_get_voltage(ADC1_CHANNEL_0), baseLine, 4095, 0, 127));
+
+	int mappedRaw = mapToCurve(adc1_get_voltage(ADC1_CHANNEL_0));
+	Serial.printf("%u - %d - %d\n", 
+		adc1_get_voltage(ADC1_CHANNEL_0), 
+		mappedRaw,
+		map(adc1_get_voltage(ADC1_CHANNEL_0), baseLine, 4095, 0, 127));
+
+
 	delay(200);
+}
+
+// http://forum.arduino.cc/index.php?topic=145443.0
+int mapToCurve(unsigned long raw) {
+	unsigned long reading = raw;
+	reading = reading * reading; // 4096*4096 = 16,777,216
+	reading = reading / (16777216/127);
+	return reading;
 }
 
 int getBaslinePressure() {
